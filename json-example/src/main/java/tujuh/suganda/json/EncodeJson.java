829e8304d91c52507c908d5f468d65c1304771f3
ridergalau <sulung.roll7@gmail.com>
2017-09-13 22:32:39 +0700

    Catatan Json Encode

---
+++ json-example/src/main/java/tujuh/suganda/json/EncodeJson.java
@@ -0,0 +1,44 @@
+package tujuh.suganda.json;
+
+import org.codehaus.jackson.map.ser.std.ObjectArraySerializer;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+public class EncodeJson {
+	public static void main(String[] args) {
+		JSONObject headObj = new JSONObject();
+		JSONObject objPrimary = new JSONObject();
+		JSONArray child = new JSONArray();
+		child.add("Rena");
+		child.add("Reni");
+		child.add("Roro");
+
+		JSONObject obj1 = new JSONObject();
+		obj1.put("mother", "Mother Rider");
+		obj1.put("father", "Father Rider");
+		obj1.put("child", child);
+
+		JSONObject objAccount = new JSONObject();
+		objAccount.put("facebook", "@Ridergalau");
+		objAccount.put("twitter", "@RiderNotGalau");
+
+		JSONArray array = new JSONArray();
+		array.add("salsa");
+		array.add("isabela");
+		array.add("rona");
+		array.add(87897);
+		array.add(objAccount);
+
+		objPrimary.put("firstname", "Rider");
+		objPrimary.put("lastname", "galau");
+		objPrimary.put("address", "Ciamis numb 7");
+		objPrimary.put("email", "galaurider@mailX.com");
+		objPrimary.put("friends", array);
+		objPrimary.put("parents", obj1);
+
+		headObj.put("data", objPrimary);
+
+		System.out.println(headObj);
+
+	}
+}
